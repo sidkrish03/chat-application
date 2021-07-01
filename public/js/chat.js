e7e98c1cbfca5c78351d2e590d1bffbe41e21138
sidkrish03 <ss3875@njit.edu>
2021-07-01 13:29:29 -0500

    Added a feature to send and receieve same chat messages among all clients

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,14 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count)
+socket.on('message', (message) => {
+    console.log(message)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked')
-    socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    // const message = document.querySelector('input').value // Method 1 - to send message without 'name' property in form input
+    const message = event.target.elements.message.value // Method 2 - to send message with 'name' property in form input
+
+    socket.emit('sendMessage', message)
 })
--- src/index.js
+++ src/index.js
@@ -12,20 +12,13 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
-// server (emit) -> client (receive) - countUpdated
-// client (emit) -> server (receive) - increment 
-
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
 
-    socket.on('increment', () => {
-        count++
-        // socket.emit('countUpdated', count) // Emits the event to only one connection
-        io.emit('countUpdated', count) // Emits the event to all connections
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
 })
 
