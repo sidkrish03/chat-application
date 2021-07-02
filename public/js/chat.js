8d88924104e8f8533e72282f82751b30d0eb13ed
sidkrish03 <ss3875@njit.edu>
2021-07-02 16:18:04 -0700

    Added functionality to render location messages dynamically

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,12 +9,22 @@ const $messages = document.querySelector('#messages')
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
 
     const html = Mustache.render(messageTemplate, {
         message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (mapsUrl) => {
+    console.log(mapsUrl)
+
+    const html = Mustache.render(locationMessageTemplate, {
+        mapsUrl
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
