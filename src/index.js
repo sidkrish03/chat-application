20e294bdd6e945c66105168a63c2503bb03f2c4a
sidkrish03 <ss3875@njit.edu>
2021-07-03 11:29:55 -0700

    Enabled tracking users joining and leaving rooms

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users.js')
 
 const app = express()
 const server = http.createServer(app)
@@ -19,15 +20,19 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     // When a client gets connected
+    socket.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room })
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+        if(error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
 
         socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
 
-        // socket.emit, io.emit, socket.broadcast.emit
-        // io.to.emit, socket.broadcast.to.emit -> allows to send a message to everyone in a specific chat room
+        callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
@@ -48,7 +53,11 @@ io.on('connection', (socket) => {
 
     // When a client gets disconnected
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+        }
     }) 
 })
 
